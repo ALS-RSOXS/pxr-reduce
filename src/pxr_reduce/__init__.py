9a203998074091e7cce718b73f613c4822d3ff54
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2026-04-30 17:12:30 -0700

    docs: update module docstring to describe PRSoXR data loader for beamline 11.0.1.2

--- src/pxr_reduce/__init__.py
+++ src/pxr_reduce/__init__.py
@@ -1,4 +1,12 @@
-""" """
+"""
+Reducing pxr data from bl 11.0.1.2
+
+This package provides a loader for PRSoXR data from beamline 11.0.1.2 taken with the
+area detector camera.
+"""
+
+from pxr_reduce.loader import PrsoxrLoader
 
 __author__ = "Thomas Ferron"
 __email__ = "tjferron@lbl.gov"
+__all__ = ["PrsoxrLoader"]
